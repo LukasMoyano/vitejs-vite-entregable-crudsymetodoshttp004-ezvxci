cf36def0636434c6cbc4610952f16cfd5255aa59
LukasMoyano <130582311+LukasMoyano@users.noreply.github.com>
2023-06-21 05:38:55 -0500

    3 files modified

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,27 @@
+import React from 'react';
+import { format, differenceInYears, getDayOfYear } from 'date-fns';
+import es from 'date-fns/locale/es';
+
 const User = ({ user }) => {
+  const { birthday } = user;
+  const parsedBirthday = new Date(birthday);
+  const formattedBirthday = format(parsedBirthday, 'EEEE d \'de\' MMMM', { locale: es });
+  const age = differenceInYears(new Date(), parsedBirthday);
+  const birthdayDayOfYear = getDayOfYear(parsedBirthday);
+
   return (
     <div>
-      <p>Nombre: {user.first_name}</p>
-      <p>Apellidos: {user.last_name}</p>
-      {/* Mostrar otros detalles del usuario */}
+      <p>
+        {user.first_name} {user.last_name}
+      </p>
+      <p>
+        Edad: {age} años
+      </p>
+      <p>
+        Cumpleaños: {formattedBirthday}
+      </p> 
+      <button>Eliminia</button>
+      <button>editar informacion</button>
     </div>
   );
 };
